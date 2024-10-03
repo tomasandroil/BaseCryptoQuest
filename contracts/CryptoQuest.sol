5868ef69b55d37765cf6b770a9701769028bcade
tomasandroil <tomasandroil@gmail.com>
2024-10-03 13:57:05 +0200

    Create CryptoQuest.sol

---
+++ contracts/CryptoQuest.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CryptoQuest is ERC721, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("CryptoQuest", "CQST") {
+        tokenCounter = 0;
+    }
+
+    function mintNFT(address to, string memory tokenURI) public onlyOwner returns (uint256) {
+        uint256 newItemId = tokenCounter;
+        _safeMint(to, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        tokenCounter += 1;
+        return newItemId;
+    }
+}
